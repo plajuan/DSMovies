05ece01c67eaa831b4220bd05414ef1e49fc109c
plajuan <pla.juan@gmail.com>
2022-01-13 09:59:45 -0300

    Save score

--- backend/src/main/java/com/devsuperior/dsmovie/service/MovieService.java
+++ backend/src/main/java/com/devsuperior/dsmovie/service/MovieService.java
@@ -1,7 +1,5 @@
 package com.devsuperior.dsmovie.service;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;

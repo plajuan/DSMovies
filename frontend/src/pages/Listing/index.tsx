da64c96d5604c651e0547bbc354eea0febbe5c87
plajuan <pla.juan@gmail.com>
2022-01-14 12:45:34 -0300

    Props

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -9,25 +9,38 @@ import { BASE_URL } from "utils/requests";
 function Listing() {
     //valor inicial do estado será zero
     const [pageNumber, setPageNumber] = useState(0);
+
+    const [page, setPage] = useState<MoviePage>({
+        content: [],
+        last: true,
+        totalPages: 0,
+        totalElements: 0,
+        size: 12,
+        number: 0,
+        first: true,
+        numberOfElements: 0,
+        empty: true
+    });
+
     //a função abaixo será executada () sempre que alguma variável [] mude seu valor
     useEffect(() => {
-        axios.get(`${BASE_URL}/movies?size=12&page=1`)
+        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}`)
             .then(res => {
                 const data = res.data as MoviePage;
-                setPageNumber(data.number);
-            })
-    }, []);
+                setPage(data);
+            });
+    }, [pageNumber]);
 
     return (
         <>
             <Pagination />
             <div className="container">
                 <div className="row">
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3"><MovieCard /></div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3"><MovieCard /></div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3"><MovieCard /></div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3"><MovieCard /></div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3"><MovieCard /></div>
+                    {page.content.map(item => 
+                        (
+                        <div key={item.id} className="col-sm-6 col-lg-4 col-xl-3 mb-3"><MovieCard movie={item} /></div>
+                        )
+                    )}
                 </div>
             </div>
         </>
